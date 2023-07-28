bd9906e4e742b62c8d27c50784c70f8910d9c6d2
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-07-28 13:54:57 +0200

    Replaced redundant code with reusable components on pages with Feed
    Created HomeScreen and CommunityScreen and used in pages with Feed

--- src/app/t/[name]/page.tsx
+++ src/app/t/[name]/page.tsx
@@ -1,62 +1,42 @@
+import Feed from '@/components/Feed/Feed';
 import HomeCTA from '@/components/HomeCTA/HomeCTA';
-import Post from '@/components/Post/Post';
+import { gql } from '@apollo/client';
 import React from 'react';
 
-const page = async ({ params: { name } }: { params: { name: string } }) => {
-	const posts = await prisma?.post.findMany({
-		where: {
-			community: {
-				topic: {
-					name,
-				},
-			},
-		},
-		include: {
-			author: true,
-			comments: {
-				include: {
-					replies: true,
-				},
-			},
-			votes: true,
-			community: true,
-		},
-	});
+const TOP_POSTS_QUERY = (topicName: string) => gql`
+	query ($offset: Int, $limit: Int) {
+		posts(
+			offset: $offset
+			limit: $limit
+			filter: { community: { topic: { name: ${`"${topicName}"`} } } }
+		) {
+			id
+			title
+			content
+			createdAt
+			comments {
+				id
+			}
+			votes {
+				userId
+				value
+			}
+			author {
+				name
+			}
+			community {
+				name
+			}
+		}
+	}
+`;
 
+const page = async ({ params: { name } }: { params: { name: string } }) => {
 	return (
 		<div className="flex-1 bg-background-feed min-h-[calc(100vh-48px)]">
 			<div className="flex gap-6 justify-center sm:px-6 pt-6">
 				<div className="w-full lg:max-w-[640px]">
-					{posts && posts?.length > 0 ? (
-						posts.map(
-							({
-								id,
-								author,
-								content,
-								title,
-								createdAt,
-								votes,
-								comments,
-								community,
-							}) => (
-								<Post
-									key={id}
-									id={id}
-									authorName={author.name}
-									content={content}
-									title={title}
-									createdAt={createdAt}
-									votes={votes}
-									comments={comments}
-									communityName={community.name}
-								/>
-							)
-						)
-					) : (
-						<div className="bg-background-primary p-2 rounded">
-							No posts in this topic yet.
-						</div>
-					)}
+					<Feed query={TOP_POSTS_QUERY(name)} />
 				</div>
 				<div className="w-[312px] hidden lg:block flex-shrink-0">
 					<HomeCTA />
