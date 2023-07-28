c3a222c35f34573959df41e00b0b1a1b686a5322
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-07-28 20:33:55 +0200

    Used new CommunityMembershipButton component

--- src/components/CommunityHeader/CommunityHeader.tsx
+++ src/components/CommunityHeader/CommunityHeader.tsx
@@ -2,20 +2,13 @@
 
 import Image from 'next/image';
 import React from 'react';
-import Button from '../buttons/Button/Button';
-import { useParams } from 'next/navigation';
-import { useSession } from 'next-auth/react';
-import { User } from '@prisma/client';
+import CommunityMembershipButton from '../CommunityMembershipButton/CommunityMembershipButton';
 
 interface CommunityHeaderProps {
-	members: User[];
+	name: string;
 }
 
-const CommunityHeader = ({ members }: CommunityHeaderProps) => {
-	const params = useParams();
-	const name = params.name as string;
-	const { data: session } = useSession();
-
+const CommunityHeader = ({ name }: CommunityHeaderProps) => {
 	return (
 		<div className="bg-background-primary">
 			<div className="h-16 bg-blue-400"></div>
@@ -29,14 +22,7 @@ const CommunityHeader = ({ members }: CommunityHeaderProps) => {
 							<div className="text-2xl font-bold">{name}</div>
 							<div className="text-sm text-text-gray">r/{name}</div>
 						</div>
-						<Button
-							text={
-								members.some((member) => member.name === session?.user.name)
-									? 'Joined'
-									: 'Join'
-							}
-							width="w-24"
-						/>
+						<CommunityMembershipButton name={name} />
 					</div>
 				</div>
 			</div>
