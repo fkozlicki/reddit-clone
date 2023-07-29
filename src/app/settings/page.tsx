0c7827302876c9642700dc85b096d3d6d9ab1895
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-07-30 00:28:56 +0200

    Created new settings page

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,38 @@
+import { getServerSession } from 'next-auth';
+import React from 'react';
+import { authOptions } from '../api/auth/[...nextauth]/route';
+import { notFound, redirect } from 'next/navigation';
+import UserSettings from '@/components/UserSettings/UserSettings';
+
+const page = async () => {
+	const session = await getServerSession(authOptions);
+
+	if (!session) {
+		redirect('/');
+	}
+
+	const user = await prisma?.user.findUnique({
+		where: {
+			id: session.user.id,
+		},
+	});
+
+	if (!user) {
+		return notFound();
+	}
+
+	const { name, displayName, about } = user;
+
+	return (
+		<div className="m-auto w-full">
+			<div className="p-5 font-medium text-lg max-w-[1200px] m-auto border-b border-border-input mb-12">
+				User settings
+			</div>
+			<div className="max-w-[1200px] m-auto">
+				<UserSettings name={name} displayName={displayName} about={about} />
+			</div>
+		</div>
+	);
+};
+
+export default page;
