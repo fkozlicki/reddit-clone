79949224a9ce5ae05b4d1185b8c400e266a61196
Filip Koźlicki <filip.kozlickii@gmail.com>
2024-01-07 22:22:18 +0100

    Adjusted ChooseCommunity dropdown

--- src/components/submit/ChooseCommunity/ChooseCommunity.tsx
+++ src/components/submit/ChooseCommunity/ChooseCommunity.tsx
@@ -1,34 +1,42 @@
 'use client';
 
+import Avatar from '@/components/ui/Avatar/Avatar';
+import Dropdown from '@/components/ui/Dropdown/Dropdown';
 import useUserCommunities from '@/hooks/query/useUserCommunities';
-import { useClickAway } from '@/hooks/useClickAway';
 import { ChevronDownIcon } from '@heroicons/react/24/solid';
 import { Community } from '@prisma/client';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import Link from 'next/link';
 
 interface ChooseCommunityProps {
 	community?: Community;
 }
 
 const ChooseCommunity = ({ community }: ChooseCommunityProps) => {
-	const { push } = useRouter();
-	const [dropdownOpen, setDropdownOpen] = useState(false);
 	const { data, loading, error } = useUserCommunities();
-	const dropdown = useClickAway<HTMLDivElement>(() => {
-		setDropdownOpen(false);
-	});
 
-	const toggleDropdown = () => {
-		setDropdownOpen((prev) => !prev);
-	};
+	const items = loading
+		? [{ text: 'Loading data' }]
+		: error
+		? [{ text: "Couldn't load data" }]
+		: data!.user.communities.map(({ name, members }) => ({
+				text: (
+					<Link href={`/r/${name}/submit`}>
+						<div className="flex items-center gap-3">
+							<Avatar size={32} />
+							<div>
+								<div className="text-sm text-primary font-medium">{name}</div>
+								<div className="text-xs text-primary">
+									{members.length} members
+								</div>
+							</div>
+						</div>
+					</Link>
+				),
+		  }));
 
 	return (
-		<div ref={dropdown} className="relative w-[275px] bg-primary rounded">
-			<div
-				onClick={toggleDropdown}
-				className="flex items-center justify-between p-2 mb-2 border border-input rounded"
-			>
+		<Dropdown items={items} className="w-full">
+			<div className="flex items-center justify-between p-2 mb-2 border border-input rounded bg-primary cursor-pointer w-[275px]">
 				<div className="flex gap-2">
 					{community ? (
 						<div className="w-5 h-5 bg-black rounded-full" />
@@ -39,35 +47,9 @@ const ChooseCommunity = ({ community }: ChooseCommunityProps) => {
 						{community?.name ?? 'Choose a community'}
 					</div>
 				</div>
-				<ChevronDownIcon width={16} className="text-primary cursor-pointer" />
+				<ChevronDownIcon width={16} className="text-primary" />
 			</div>
-			{dropdownOpen && (
-				<div className="absolute top-full left-0 bg-primary w-full p-2 drop-shadow-md">
-					{data && (
-						<>
-							<div className="text-[10px] font-bold uppercase text-primary mb-2">
-								Your communities
-							</div>
-							{data.user.communities.map(({ id, name, members }) => (
-								<div
-									onClick={() => push(`/r/${name}/submit`)}
-									key={id}
-									className="flex gap-2 cursor-pointer"
-								>
-									<div className="w-8 h-8 bg-black rounded-full items-center"></div>
-									<div>
-										<div className="text-sm text-primary">{name}</div>
-										<div className="text-xs text-primary">
-											{members.length} members
-										</div>
-									</div>
-								</div>
-							))}
-						</>
-					)}
-				</div>
-			)}
-		</div>
+		</Dropdown>
 	);
 };
 
