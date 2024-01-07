2e497223da60f11c637bf4ff01af9551de1f1951
Filip Koźlicki <filip.kozlickii@gmail.com>
2024-01-07 11:22:38 +0100

    Removed unused gql query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,4 @@
-import { gql } from '@apollo/client';
 import HomeScreen from '@/components/HomeScreen/HomeScreen';
-
-const POSTS_QUERY = gql`
-	query bestPosts($offset: Int, $limit: Int) {
-		posts(offset: $offset, limit: $limit) {
-			id
-			title
-			content
-			createdAt
-			comments {
-				id
-			}
-			votes {
-				userId
-				value
-			}
-			author {
-				name
-			}
-			community {
-				name
-			}
-		}
-	}
-`;
 
 export default function Home() {
 	return <HomeScreen feedType="best" highlighted="best" />;
