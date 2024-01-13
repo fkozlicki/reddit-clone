993c385689541c42b96988bd24114fc1043b47e3
Filip Koźlicki <filip.kozlickii@gmail.com>
2024-01-14 00:17:37 +0100

    Created Tab component

---
+++ src/components/ui/Tab/Tab.tsx
@@ -0,0 +1,25 @@
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import React, { ReactNode } from 'react';
+
+interface TabProps {
+	href: string;
+	selected: boolean;
+	children: ReactNode;
+}
+
+const Tab = ({ href, selected, children }: TabProps) => {
+	return (
+		<Link
+			href={href}
+			className={cn(
+				'flex items-center h-full px-2 uppercase text-sm font-medium',
+				{ 'shadow-tab text-button': selected }
+			)}
+		>
+			{children}
+		</Link>
+	);
+};
+
+export default Tab;
