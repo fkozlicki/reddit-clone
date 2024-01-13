175d16bc9f9a6421dad00e5926a56763cf9f6f23
Filip Koźlicki <filip.kozlickii@gmail.com>
2024-01-14 00:17:54 +0100

    Created UserAbout component

---
+++ src/components/profile/UserAbout/UserAbout.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import Avatar from '@/components/ui/Avatar/Avatar';
+import Button from '@/components/ui/Button/Button';
+import { formatDate } from '@/utils/formatDate';
+import { User } from '@prisma/client';
+import { useSession } from 'next-auth/react';
+import Link from 'next/link';
+import React from 'react';
+
+interface UserAboutProps {
+	user: User;
+}
+
+const UserAbout = ({ user }: UserAboutProps) => {
+	const { data: session } = useSession();
+
+	return (
+		<div className="bg-primary rounded overflow-hidden text-primary border border-post">
+			<div className="h-20 bg-button"></div>
+			<div className="flex justify-center -mt-[50px]">
+				<Avatar size={100} />
+			</div>
+			<div className="p-4">
+				<div className="text-2xl font-medium text-center">
+					{user.displayName ?? user.name}
+				</div>
+				<div className="text-xs font-medium text-gray-500 text-center mb-4">
+					u/{user.name}
+				</div>
+				<div className="flex">
+					<div className="flex-1">
+						<div className="text-sm font-medium mb-1">Karma</div>
+						<div className="text-xs text-gray-500">222</div>
+					</div>
+					<div className="flex-1">
+						<div className="text-sm font-medium mb-1">Joined</div>
+						<div className="text-xs text-gray-500">
+							{formatDate(new Date(user.createdAt))}
+						</div>
+					</div>
+				</div>
+				{user.name === session?.user.name && (
+					<>
+						<div className="h-4" />
+						<Link href="/submit">
+							<Button variant="primary" className="w-full">
+								Create Post
+							</Button>
+						</Link>
+					</>
+				)}
+			</div>
+		</div>
+	);
+};
+
+export default UserAbout;
