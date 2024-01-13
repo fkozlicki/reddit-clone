bb87e06176049bccc16e78f7f2c93fa125251563
Filip Koźlicki <filip.kozlickii@gmail.com>
2024-01-14 00:16:43 +0100

    Created useCommunities query hook

---
+++ src/hooks/query/useCommunities.ts
@@ -0,0 +1,34 @@
+import { QueryHookOptions, gql, useQuery } from '@apollo/client';
+
+type CommunitiesQueryResponse = {
+	communities: {
+		id: string;
+		name: string;
+		membersCount: number;
+	}[];
+};
+
+type CommunitiesQueryVariables = {
+	filter?: any;
+	sort?: string;
+	take?: number;
+};
+
+const COMMUNITIES_QUERY = gql`
+	query Communities($filter: CommunityFilter, $take: Int, $sort: String) {
+		communities(filter: $filter, take: $take, sort: $sort) {
+			id
+			name
+			membersCount
+		}
+	}
+`;
+
+export default function useCommunities(
+	options: QueryHookOptions<CommunitiesQueryResponse, CommunitiesQueryVariables>
+) {
+	return useQuery<CommunitiesQueryResponse, CommunitiesQueryVariables>(
+		COMMUNITIES_QUERY,
+		options
+	);
+}
