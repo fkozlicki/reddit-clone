935af02ce6edbee1034903a6ba5e92317d5be8b0
Filip Koźlicki <filip.kozlickii@gmail.com>
2024-01-14 00:15:13 +0100

    Created UserPosts page

---
+++ src/app/user/[name]/posts/page.tsx
@@ -0,0 +1,10 @@
+import Feed from '@/components/feed-layout/Feed/Feed';
+import React from 'react';
+
+export default function UserPosts({
+	params: { name },
+}: {
+	params: { name: string };
+}) {
+	return <Feed type="hot" authorName={name} />;
+}
