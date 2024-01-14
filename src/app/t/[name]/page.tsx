6b29697442b95dfff12add7340376271c600d1ac
Filip Koźlicki <filip.kozlickii@gmail.com>
2024-01-14 18:27:44 +0100

    Added not found in topic

--- src/app/t/[name]/page.tsx
+++ src/app/t/[name]/page.tsx
@@ -1,9 +1,19 @@
 import Feed from '@/components/feed-layout/Feed/Feed';
 import TopCommunities from '@/components/topic/TopCommunities/TopCommunities';
 import Grid from '@/components/ui/Grid/Grid';
+import { prisma } from '@/lib/prisma';
+import { notFound } from 'next/navigation';
 import React from 'react';
 
 const page = async ({ params: { name } }: { params: { name: string } }) => {
+	const topic = await prisma.topic.findUnique({
+		where: { name },
+	});
+
+	if (!topic) {
+		notFound();
+	}
+
 	return (
 		<div className="py-6">
 			<Grid
