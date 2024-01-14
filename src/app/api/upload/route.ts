82d7a1af063c7da200cb86375eae97ee71855b15
Filip Koźlicki <filip.kozlickii@gmail.com>
2024-01-14 21:15:44 +0100

    Added authorization to image upload

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,9 +1,18 @@
 import S3 from 'aws-sdk/clients/s3';
+import { getServerSession } from 'next-auth';
 import { NextRequest, NextResponse } from 'next/server';
+import { authOptions } from '../auth/[...nextauth]/route';
 
 export async function PUT(req: NextRequest, res: NextResponse) {
+	const session = getServerSession(authOptions);
+
+	if (!session) {
+		throw new Error('You have to be logged in');
+	}
+
 	const file = req.nextUrl.searchParams.get('file');
 	const fileType = req.nextUrl.searchParams.get('fileType');
+	const folder = req.nextUrl.searchParams.get('folder');
 
 	try {
 		const s3 = new S3({
@@ -14,7 +23,7 @@ export async function PUT(req: NextRequest, res: NextResponse) {
 		});
 
 		const preSignedUrl = s3.getSignedUrl('putObject', {
-			Bucket: process.env.AWS_BUCKET_NAME,
+			Bucket: `${process.env.AWS_BUCKET_NAME}${folder}`,
 			Key: file,
 			ContentType: fileType,
 			Expires: 5 * 60,
