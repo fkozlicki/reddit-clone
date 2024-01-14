a37f75df37cdf24e23cebe88dd5949ec107fb5f6
Filip Koźlicki <filip.kozlickii@gmail.com>
2024-01-14 16:13:29 +0100

    Moved providers to Providers component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,13 @@
 import Providers from '@/components/Providers';
-import './globals.css';
-import { Inter } from 'next/font/google';
 import Navbar from '@/components/layout/Navbar/Navbar';
-import { Toaster } from 'react-hot-toast';
-import { getServerSession } from 'next-auth';
-import { authOptions } from './api/auth/[...nextauth]/route';
 import Sidebar from '@/components/layout/Sidebar/Sidebar';
-import { ReactNode } from 'react';
-import ModalsProvider from '@/contexts/ModalsContext';
-import ThemeProvider from '@/contexts/ThemeContext';
 import { cn } from '@/lib/utils';
+import { getServerSession } from 'next-auth';
+import { Inter } from 'next/font/google';
+import { ReactNode } from 'react';
+import { Toaster } from 'react-hot-toast';
+import { authOptions } from './api/auth/[...nextauth]/route';
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -28,29 +26,25 @@ export default async function RootLayout({
 	return (
 		<html lang="en">
 			<Providers>
-				<ThemeProvider>
-					<ModalsProvider>
-						<body className={inter.className}>
-							<Toaster position="top-center" />
-							<div className="fixed w-full top-0 z-20">
-								<Navbar session={session} />
+				<body className={inter.className}>
+					<Toaster position="top-center" />
+					<div className="fixed w-full top-0 z-20">
+						<Navbar session={session} />
+					</div>
+					<main
+						className={cn('pt-12 min-h-screen bg-secondary', {
+							'xl:pl-[270px] flex': !session,
+						})}
+					>
+						{!session && (
+							<div className="hidden xl:block fixed top-12 left-0">
+								<Sidebar />
 							</div>
-							<main
-								className={cn('pt-12 min-h-screen bg-secondary', {
-									'xl:pl-[270px] flex': !session,
-								})}
-							>
-								{!session && (
-									<div className="hidden xl:block fixed top-12 left-0">
-										<Sidebar />
-									</div>
-								)}
-								<div className="flex-1">{children}</div>
-							</main>
-							<div id="portal" />
-						</body>
-					</ModalsProvider>
-				</ThemeProvider>
+						)}
+						<div className="flex-1">{children}</div>
+					</main>
+					<div id="portal" />
+				</body>
 			</Providers>
 		</html>
 	);
--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -4,11 +4,17 @@ import { SessionProvider } from 'next-auth/react';
 import React, { ReactNode } from 'react';
 import { ApolloProvider } from '@apollo/client';
 import apolloClient from '@/lib/apollo';
+import ThemeProvider from '@/contexts/ThemeContext';
+import ModalsProvider from '@/contexts/ModalsContext';
 
 const Providers = ({ children }: { children: ReactNode }) => {
 	return (
 		<SessionProvider>
-			<ApolloProvider client={apolloClient}>{children}</ApolloProvider>
+			<ApolloProvider client={apolloClient}>
+				<ThemeProvider>
+					<ModalsProvider>{children}</ModalsProvider>
+				</ThemeProvider>
+			</ApolloProvider>
 		</SessionProvider>
 	);
 };
