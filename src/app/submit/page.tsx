9b67d469f03f659824637f2d8eb930766393d0c9
Filip Koźlicki <filip.kozlickii@gmail.com>
2024-01-28 16:46:59 +0100

    Fixed submit page layout

--- src/app/submit/page.tsx
+++ src/app/submit/page.tsx
@@ -4,6 +4,7 @@ import PostForm from '@/components/submit/PostForm/PostForm';
 import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
 import { authOptions } from '../api/auth/[...nextauth]/route';
+import Grid from '@/components/ui/Grid/Grid';
 
 export const metadata = {
 	title: 'Submit to Reddit',
@@ -18,15 +19,17 @@ export default async function Submit() {
 
 	return (
 		<div className="flex justify-center gap-6 pt-6">
-			<div className="w-full lg:w-auto lg:min-w-[640px]">
-				<div className="mb-4 font-medium text-primary">Create a post</div>
-				<div className="w-full h-px bg-primary my-4" />
-				<ChooseCommunity />
-				<PostForm />
-			</div>
-			<div className="w-[312px] hidden lg:block flex-shrink-0">
-				<PostRules />
-			</div>
+			<Grid
+				left={
+					<>
+						<div className="mb-4 font-medium text-primary">Create a post</div>
+						<div className="w-full h-px bg-primary my-4" />
+						<ChooseCommunity />
+						<PostForm />
+					</>
+				}
+				right={<PostRules />}
+			/>
 		</div>
 	);
 }
