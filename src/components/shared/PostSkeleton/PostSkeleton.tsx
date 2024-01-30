3654fedb8086864f1275f21d2bc6b9a4dc2c1682
Filip Koźlicki <filip.kozlickii@gmail.com>
2024-01-30 20:04:06 +0100

    Removed VoteSection from PostSkeleton component

--- src/components/shared/PostSkeleton/PostSkeleton.tsx
+++ src/components/shared/PostSkeleton/PostSkeleton.tsx
@@ -1,18 +1,7 @@
-import React from 'react';
-import VoteSection from '../../shared/VoteSection/VoteSection';
-
 const PostSkeleton = () => {
 	return (
 		<div className="w-full flex border-post border rounded hover:post-hover cursor-pointer overflow-hidden">
-			<div className="bg-post-side p-2 hidden sm:block">
-				<VoteSection
-					type="post"
-					postId=""
-					direction="column"
-					karma={0}
-					voteValue={null}
-				/>
-			</div>
+			<div className="bg-post-side w-10 hidden sm:block"></div>
 			<div className="bg-primary flex-1 p-2">
 				<div className="animate-pulse">
 					<div className="h-4 bg-gray-300 rounded mb-4 max-w-[250px]"></div>
