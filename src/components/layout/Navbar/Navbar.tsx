3751870f62382eff1a70d541ac5e87d13a4341bf
Filip Koźlicki <filip.kozlickii@gmail.com>
2024-03-27 17:36:54 +0100

    Added chat icon to navbar

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -4,11 +4,12 @@ import Logo from '@/components/layout/Logo/Logo';
 import Avatar from '@/components/ui/Avatar/Avatar';
 import Button from '@/components/ui/Button/Button';
 import Dropdown from '@/components/ui/Dropdown/Dropdown';
-import TextField from '@/components/ui/TextField/TextField';
+import { useChatContext } from '@/contexts/ChatContext';
 import { useModalsContext } from '@/contexts/ModalsContext';
 import { useThemeContext } from '@/contexts/ThemeContext';
 import {
 	ArrowRightOnRectangleIcon,
+	ChatBubbleOvalLeftEllipsisIcon,
 	ChevronDownIcon,
 	Cog6ToothIcon,
 	MoonIcon,
@@ -28,6 +29,7 @@ interface NavbarProps {
 const Navbar = ({ session }: NavbarProps) => {
 	const [{ signIn }, dispatch] = useModalsContext();
 	const { theme, changeTheme } = useThemeContext();
+	const [, dispatchChat] = useChatContext();
 
 	const openModal = () => {
 		dispatch({ type: 'openSignIn' });
@@ -39,6 +41,10 @@ const Navbar = ({ session }: NavbarProps) => {
 
 	const toggleTheme = () => {
 		changeTheme(theme === 'light' ? 'dark' : 'light');
+	};
+
+	const openChat = () => {
+		dispatchChat({ type: 'setOpen', payload: true });
 	};
 
 	return (
@@ -53,58 +59,64 @@ const Navbar = ({ session }: NavbarProps) => {
 				</Button>
 			)}
 			{session && (
-				<Dropdown
-					className="min-w-[200px]"
-					items={[
-						{
-							text: (
-								<Link
-									className="flex gap-3 items-center"
-									href={`/user/${session.user.name}`}
-								>
-									<UserIcon width={20} />
-									<span>Profile</span>
-								</Link>
-							),
-						},
-						{
-							text: (
-								<Link className="flex gap-3 items-center" href="/settings">
-									<Cog6ToothIcon width={20} />
-									<span>Settings</span>
-								</Link>
-							),
-						},
-						{
-							text: theme === 'light' ? 'Dark Mode' : 'Light Mode',
-							icon:
-								theme === 'light' ? (
-									<MoonIcon width={20} />
-								) : (
-									<SunIcon width={20} />
+				<div className="flex items-center gap-4">
+					<Button
+						onClick={openChat}
+						icon={<ChatBubbleOvalLeftEllipsisIcon width={20} />}
+					/>
+					<Dropdown
+						className="min-w-[200px]"
+						items={[
+							{
+								text: (
+									<Link
+										className="flex gap-3 items-center"
+										href={`/user/${session.user.name}`}
+									>
+										<UserIcon width={20} />
+										<span>Profile</span>
+									</Link>
 								),
-							onClick: toggleTheme,
-						},
-						{
-							text: 'Log out',
-							icon: <ArrowRightOnRectangleIcon width={20} />,
-							onClick: signOut,
-						},
-					]}
-				>
-					<div className="flex gap-4 p-1 rounded hover:bg-btn-text cursor-pointer">
-						<div className="flex items-center gap-2">
-							<Avatar url={session.user.image} alt="avatar" size={32} />
-							<div className="flex flex-col items-start">
-								<div className="text-xs font-semibold max-w-[100px] overflow-hidden text-ellipsis whitespace-nowrap text-primary">
-									{session.user.name}
+							},
+							{
+								text: (
+									<Link className="flex gap-3 items-center" href="/settings">
+										<Cog6ToothIcon width={20} />
+										<span>Settings</span>
+									</Link>
+								),
+							},
+							{
+								text: theme === 'light' ? 'Dark Mode' : 'Light Mode',
+								icon:
+									theme === 'light' ? (
+										<MoonIcon width={20} />
+									) : (
+										<SunIcon width={20} />
+									),
+								onClick: toggleTheme,
+							},
+							{
+								text: 'Log out',
+								icon: <ArrowRightOnRectangleIcon width={20} />,
+								onClick: signOut,
+							},
+						]}
+					>
+						<div className="flex gap-4 p-1 rounded hover:bg-btn-text cursor-pointer">
+							<div className="flex items-center gap-2">
+								<Avatar url={session.user.image} alt="avatar" size={32} />
+								<div className="flex flex-col items-start">
+									<div className="text-xs font-semibold max-w-[100px] overflow-hidden text-ellipsis whitespace-nowrap text-primary">
+										{session.user.name}
+									</div>
+									<div className="text-xs text-start text-primary">1 karma</div>
 								</div>
-								<div className="text-xs text-start text-primary">1 karma</div>
 							</div>
+							<ChevronDownIcon width={16} className="text-primary" />
 						</div>
-						<ChevronDownIcon width={16} className="text-primary" />
-					</div>
-				</Dropdown>
+					</Dropdown>
+				</div>
 			)}
 			<AuthModal open={!session && signIn} onClose={closeModal} />
 		</div>
