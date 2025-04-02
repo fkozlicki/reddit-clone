a762a807b2c8091bc1278d002b5d01d00b3dd00a
Filip Koźlicki <filip.kozlickii@gmail.com>
2025-04-02 12:41:07 +0200

    next.config update

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	experimental: {
-		swcPlugins: [['next-superjson-plugin', {}]],
-	},
 	images: {
-		domains: [
-			'lh3.googleusercontent.com',
-			'redditv2.s3.eu-central-1.amazonaws.com',
+		remotePatterns: [
+			{ hostname: 'lh3.googleusercontent.com' },
+			{ hostname: 'redditv2.s3.eu-central-1.amazonaws.com' },
 		],
 	},
 	reactStrictMode: false,
